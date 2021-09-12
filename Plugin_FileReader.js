91e023672ce23353c6cdd20ba4309f7765c1e387
雪乃☆雫 <59927104+snowdrops89@users.noreply.github.com>
2021-09-13 07:24:06 +0900

    ドラッグ＆ドロップで読み込む

--- Plugin_FileReader.js
+++ Plugin_FileReader.js
@@ -3,6 +3,31 @@
  * ローカルのファイルを読み込むためのプラグイン
  */
 const PluginFileReader = {
+  // @ドラッグ&ドロップ
+  'ファイルドロップ時': { // @DOMにファイルをドロップした時 // @てきすとふぁいるひらいたとき
+    type: 'func',
+    josi: [['と'],['に', 'へ']],
+    fn: function (fn, dom, sys) {
+      if (typeof (dom) === 'string') {dom = document.querySelector(dom)}
+      dom.ondragenter = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      }
+      dom.ondragover = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      }
+      dom.ondrop = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        const dt = e.dataTransfer;
+        const files = dt.files;
+        sys.__v0['対象'] = files
+        sys.__v0['対象イベント'] = e
+        return fn(e, sys);
+      }
+    }
+  },
   // @ファイル開く
   'テキストファイル開時': { // @ローカルのテキストファイルを開く // @てきすとふぁいるひらいたとき
     type: 'func',
